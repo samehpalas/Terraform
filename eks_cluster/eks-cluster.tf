61c4be79da4a7ac8ce2000248cbf7fb919d0e334
samehpalas <127115860+samehpalas@users.noreply.github.com>
2024-04-07 06:01:40 +0200

    Update eks-cluster.tf

--- eks_cluster/eks-cluster.tf
+++ eks_cluster/eks-cluster.tf
@@ -55,29 +55,6 @@ module "eks" {
      # }
     }
   }
-# render Admin & Developer users list with the structure required by EKS module
-#locals {
- # cluster_name = "${var.name_prefix}-${var.environment}"
 
-  #autoscaler_service_account_namespace = "kube-system"
-  #autoscaler_service_account_name      = "cluster-autoscaler-aws"
-
-admin_user_map_users = [
-    for admin_user in var.admin_users :
-    {
-      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"
-      username = admin_user
-      groups   = ["system:masters"]
-    }
-  ]
-
-developer_user_map_users = [
-    for developer_user in var.developer_users :
-    {
-      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${developer_user}"
-      username = developer_user
-      groups   = ["${var.name_prefix}-developers"]
-    }
-  ]
 }
 
