37cfad90fd0921717788b09dfe87365b6fca4803
samehpalas <127115860+samehpalas@users.noreply.github.com>
2024-04-07 05:52:16 +0200

    Update eks-cluster.tf

--- eks_cluster/eks-cluster.tf
+++ eks_cluster/eks-cluster.tf
@@ -10,6 +10,8 @@ data "aws_eks_cluster" "myapp1-cluster" {
   data "aws_eks_cluster_auth" "myapp1-cluster" {
     name = module.eks.cluster_name
   }
+data "aws_caller_identity" "current" {} # used for accessing Account ID and ARN
+
 module "eks" {
    source  = "terraform-aws-modules/eks/aws"
    version = "20.8.3"
@@ -53,4 +55,29 @@ module "eks" {
      # }
     }
   }
+# render Admin & Developer users list with the structure required by EKS module
+locals {
+  cluster_name = "${var.name_prefix}-${var.environment}"
+
+  autoscaler_service_account_namespace = "kube-system"
+  autoscaler_service_account_name      = "cluster-autoscaler-aws"
+
+  admin_user_map_users = [
+    for admin_user in var.admin_users :
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"
+      username = admin_user
+      groups   = ["system:masters"]
+    }
+  ]
+
+  developer_user_map_users = [
+    for developer_user in var.developer_users :
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${developer_user}"
+      username = developer_user
+      groups   = ["${var.name_prefix}-developers"]
+    }
+  ]
+}
 }
