0b13a3f81f632114f53a84a27a2757299783889c
samehpalas <127115860+samehpalas@users.noreply.github.com>
2024-04-07 22:40:47 +0200

    Update eks-cluster.tf

--- eks_cluster/eks-cluster.tf
+++ eks_cluster/eks-cluster.tf
@@ -1,9 +1,4 @@
-provider "kubernetes" {
-  config_paths = "false"
-  host                   =  data.aws_eks_cluster.my-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my-cluster.certificate_authority.0.data)
-  token = data.aws_eks_cluster_auth.my-cluster.token   
-}
+
 data "aws_eks_cluster" "my-cluster" {
   name = module.eks.cluster_name
 }
